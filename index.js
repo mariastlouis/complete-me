a783dfe8eda55a044f17d0aa7be393656e63008c
mariastlouis <msantray@yahoo.com>
2017-10-25 22:44:57 -0600

    preparing to export to github

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 import { bubbleSort, insertionSort, mergeSort, quickSort } from '@mariastlouis/sorting-suite'
+const Node = require('./lib/Nodel.js');
+const Trie = require('./lib/Trie.js')
+
+module.exports = { Node, Trie }
